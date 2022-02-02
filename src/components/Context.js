c49339ec3adf79926b3bec7ddf06c818f13e484e
StefanVelkoski <velkoskis@gmail.com>
2022-02-02 17:06:54 +0100

    server friendly oauth

--- src/components/Context.js
+++ src/components/Context.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import React, { createContext, useEffect, useState } from "react";
 
 
+
+
 export const myContext = createContext({});
 
 export default function Context(props){
@@ -9,7 +11,7 @@ export default function Context(props){
     const [userObject, setUserObject] = useState();
 
     useEffect (() =>{ 
-        axios.get("http://localhost:3000/getuser",{ withCredentials: true}).then(res => {
+        axios.get(`${process.env.REACT_APP_BACKEND_URL}/getuser`,{ withCredentials: true}).then(res => {
             if (res.data){
                 console.log(res)
                 setUserObject(res.data)
