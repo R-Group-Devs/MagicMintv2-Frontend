585e6a297a21e7f03cf812911ed660b452c8e478
Viktor Tasevski <viktor-tasevski@hotmail.com>
2022-05-14 15:53:14 +0200

    just to start https server on anothr pc

--- src/components/Context.js
+++ src/components/Context.js
@@ -1,28 +1,24 @@
-import axios from "axios";
-import React, { createContext, useEffect, useState } from "react";
-
-
-
-
+import axios from 'axios';
+import React, { createContext, useEffect, useState } from 'react';
 export const myContext = createContext({});
 
-export default function Context(props){
+export default function Context(props) {
+  const [userObject, setUserObject] = useState();
 
-    const [userObject, setUserObject] = useState();
+  useEffect(() => {
+    axios
+      .get(`/auth/login/success`)
+      .then((res) => {
+        console.log('axios object', res);
+        if (res.data) {
+          console.log('response data', res.data.user);
+          setUserObject(res.data.user);
+        }
+      })
+      .catch((err) => console.log({ err }));
+  }, []);
 
-
-    useEffect (() =>{ 
-        axios.get(`${process.env.REACT_APP_BACKEND_URL}/getuser`, {withCredentials: true}).then(res => {
-            console.log("axios object",res)
-            if (res.data){
-                console.log("response data",res)
-                setUserObject(res.data)
-                localStorage.setItem('profile', JSON.stringify(res.data))
-            }
-        })
-    },[])
-
-    return(
+  return (
     <myContext.Provider value={userObject}>{props.children}</myContext.Provider>
-    )
+  );
 }
--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,56 +1,55 @@
 import React, { useEffect } from 'react';
-import "./Welcome.css"
+import './Welcome.css';
 import { Link } from 'react-router-dom';
 import Header from '../Navbar/Header';
 import { myContext } from '../Context';
 import { useContext } from 'react';
 
-export default function Welcome (){
+export default function Welcome() {
+  const userObject = useContext(myContext);
 
-    
-    let userObject = localStorage.getItem('profile')
+  if (userObject) {
+    return (
+      <div className=''>
+        <Header
+          image={userObject.twitterPhoto}
+          username={userObject.username}
+        />
 
-    // useEffect(()=>{
-    //     if(!window.location.hash) {
-	// 	window.location = window.location + '#loaded';
-	// 	window.location.reload();
-	// }
-    // },[userObject])      
-
-if (userObject){
-        userObject = userObject ? JSON.parse(userObject) : []
-
-
-    return(
-
-        <div className="">
-            <Header image= {userObject.photos[0].value} username={userObject.username}/>
-
-            <div className="row welcome-wrapper">
-                <div className="col-md-4 col-lg-4 col-sm-12">
-                </div>
-                <div className="col-md-4 col-lg-4 col-sm-12 ">
-                    <div className="welcomebuttons">
-                        <div className="welcome-title">Greetings, {userObject.username} 👋  </div>
-                        <div className="welcome-desc"></div>
-                        <button className="campaign-button"><Link className="text-link" to="/createcampaign"> Create a campaign</Link></button><br></br>
-                        <button className="claim-button" disabled> <Link className="text-link" to="/claim"> Claim your nft</Link> </button>
-                    </div>
-                </div>
-                <div className="col-md-4 col-lg-4 col-sm-12 ">
-                </div>
+        <div className='row welcome-wrapper'>
+          <div className='col-md-4 col-lg-4 col-sm-12'></div>
+          <div className='col-md-4 col-lg-4 col-sm-12 '>
+            <div className='welcomebuttons'>
+              <div className='welcome-title'>
+                Greetings, {userObject?.username} 👋{' '}
+              </div>
+              <div className='welcome-desc'></div>
+              <button className='campaign-button'>
+                <Link className='text-link' to='/createcampaign'>
+                  {' '}
+                  Create a campaign
+                </Link>
+              </button>
+              <br></br>
+              <button className='claim-button' disabled>
+                {' '}
+                <Link className='text-link' to='/claim'>
+                  {' '}
+                  Claim your nft
+                </Link>{' '}
+              </button>
             </div>
+          </div>
+          <div className='col-md-4 col-lg-4 col-sm-12 '></div>
         </div>
-        )
-} else {
-    return(
-        <div className='not-logged-in'>
-            You are not logged in correctly.
-            Please head <a href='/auth'> here</a> to login with Twitter and access the app!
-        </div>
-    )
-}
-
-
-
+      </div>
+    );
+  } else {
+    return (
+      <div className='not-logged-in'>
+        You are not logged in correctly. Please head <a href='/auth'> here</a>{' '}
+        to login with Twitter and access the app!
+      </div>
+    );
+  }
 }
